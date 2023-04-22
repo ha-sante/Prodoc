8f92c616f5cb1829f528426b3a5293a73d9d1b3f
ha-sante <90225652+ha-sante@users.noreply.github.com>
2023-04-22 18:25:44 +0000

    Init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
+  // Per every page, it implements its SEO details here
+
+
   return (
     <Html lang="en">
       <Head />
---
+++ src/pages/editor.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+import sidebar from "../components/sidebar";
+import content from "../components/content";
+import navbar from "../components/navbar";
+
+export default function Editor() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+   
+    </main>
+  )
+}
