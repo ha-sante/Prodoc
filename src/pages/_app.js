b26cb8de92fdd99e499b1de9d46f94fdde354211
ha-sante <90225652+ha-sante@users.noreply.github.com>
2023-05-07 09:52:44 +0000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,13 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import HawkCatcher from '@hawk.so/javascript';
 
-if (window) {
-  const hawk = new HawkCatcher({
-    token: process.env.NEXT_PUBLIC_HAWK_PUBLIC_KEY
-  });
+if (typeof window !== "undefined") {
+  // Client-side-only code
+  if (window) {
+    const hawk = new HawkCatcher({
+      token: process.env.NEXT_PUBLIC_HAWK_PUBLIC_KEY
+    });
+  }
 }
 
 export default function App({ Component, pageProps }) {
