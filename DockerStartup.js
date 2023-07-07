5ad32e870056614cb02787ca2bd27ebb4dc6a4b0
ha-sante <90225652+ha-sante@users.noreply.github.com>
2023-07-07 23:28:59 +0000

    Update DockerStartup.js

--- DockerStartup.js
+++ DockerStartup.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 
 if (process.env.PRISMA_SQL_DATABASE_SERVICE_CONNECTION_STRING) {
-    exec(`sudo npm i -g prisma && sudo prisma migrate deploy`, (err, stdout, stderr) => {
+    exec(`npm i -g prisma && prisma migrate deploy`, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return;
